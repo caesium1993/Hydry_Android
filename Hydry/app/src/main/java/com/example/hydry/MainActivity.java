cde473c4bade2db5efe81d852710fe41881e567f
unknown <95xuhuijie@gmail.com>
2017-10-04 15:32:08 +1100

    add files

---
+++ Hydry/app/src/main/java/com/example/hydry/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.hydry;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.net.MalformedURLException;
+
+public class MainActivity extends Activity {
+
+    public static AzureServicesAdapter azureServicesAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            AzureServicesAdapter.Initialize(this);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        azureServicesAdapter=AzureServicesAdapter.getInstance();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void ToLogin(View view){
+        Intent intent=new Intent();
+        intent.setClass(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void ToRegister(View view){
+        Intent intent=new Intent();
+        intent.setClass(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
--- Hydry/app/src/main/java/com/example/hydry/SupermarketActivity.java
+++ Hydry/app/src/main/java/com/example/hydry/SupermarketActivity.java
@@ -1,7 +1,10 @@
 package com.example.hydry;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class SupermarketActivity extends Activity {
@@ -11,4 +14,54 @@ public class SupermarketActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supermarket);
     }
+
+    public void Tocoles(View v){
+        Intent intent=new Intent();
+        intent.setClass(SupermarketActivity.this, ColesActivity.class);
+        startActivity(intent);
+    }
+
+    public void Toworth(View v){
+        Intent intent=new Intent();
+        intent.setClass(SupermarketActivity.this, WorthActivity.class);
+        startActivity(intent);
+    }
+
+    public void ToVM(View v){
+        Uri uri = Uri.parse("http://www.qvm.com.au/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void Tojibihifi(View v){
+        Uri uri = Uri.parse("https://www.jbhifi.com.au/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void Toofficework(View v){
+        Uri uri = Uri.parse("https://www.officeworks.com.au/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void Tobigw(View v){
+        Uri uri = Uri.parse("https://www.bigw.com.au/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void Tokmart(View v){
+        Uri uri = Uri.parse("http://www.kmart.com.au/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void Totarget(View v){
+        Uri uri = Uri.parse("https://www.target.com.au/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
 }
